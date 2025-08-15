5987fc0fd0d4ab5986ada5b9f1a6f8ab4edc6e9f
kirbon <85096989+kirbonn@users.noreply.github.com>
2025-08-15 16:17:20 +0000

    initial commit

--- MyFrame.java
+++ MyFrame.java
@@ -10,8 +10,18 @@ public class MyFrame extends JFrame{
 
 MyFrame(){
 // Your code here.
-
-
+// Jframe is a gui window to add compontents to.
+this.setSize(450,450); // x,y window size
+this.setTitle("woof"); // this sets the title
+this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+this.setResizable(false); // Frame size is locked.
+this.setVisible(true);
+ImageIcon image = new ImageIcon("Soggy_Cat.jpeg");
+this.setIconImage(image.getImage());
+this.getContentPane().setBackground(Color.MAGENTA);
+JLabel myLabel = new JLabel("hi hello");
+this.add(myLabel);
+this.
 	
 }
 	}
