eb3353db707e3cbf7fabdf2cfbe994adb6bc91a8
kirbon <85096989+kirbonn@users.noreply.github.com>
2025-08-15 15:41:41 +0000

    Initial commit

---
+++ MyFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+
+public class MyFrame extends JFrame{
+
+//MyFrame is the subclass or child class; JFrame is the parent class or super class
+
+MyFrame(){
+// Your code here.
+
+
+	
+}
+	}
