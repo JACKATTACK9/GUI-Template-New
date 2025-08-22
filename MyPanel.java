8ae2f0990685c32d770c2e2c5598d2bb9c6cc611
kirbon <85096989+kirbonn@users.noreply.github.com>
2025-08-22 16:00:59 +0000

    commit

--- MyPanel.java
+++ MyPanel.java
@@ -10,8 +10,14 @@ import javax.swing.BorderFactory;
 
 public class MyPanel {
   MyPanel() { // All of your code today goes below this line!
+    ImageIcon image = new ImageIcon("Soggy_cat.jpeg");
+    Border border = BorderFactory.createLineBorder(Color.pink, 5); // this makes a border around the label
+    JLabel label = new JLabel();
+    label.setIcon(image);
+    label.setText("meeeeeeeerrrrrroooooooowwwwwwwww");
 
-
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
 
